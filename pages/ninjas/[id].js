2fac2e3be327841e6d3853863f17d5772819feb5
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-03-04 21:32:15 +0200

    git single ninja by id

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,9 +1,38 @@
-import React from "react";
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  // map data to an array of path objects with params (id)
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
 
-const Details = () => {
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <>
-      <h1>Details Page</h1>
+      <div>
+        <h4>Id {ninja.id}</h4>
+        <h1>name: {ninja.name}</h1>
+        <p>Email: {ninja.email}</p>
+        <p>WebSite: {ninja.website}</p>
+        <p>city: {ninja.address.city}</p>
+      </div>
     </>
   );
 };
